08a765e9c8162589c40451652844a40d67710987
Muhammad361738 <ullahehsan016@gmail.com>
2025-07-26 23:24:23 +0500

    Add day 02

---
+++ day_2/main.py
@@ -0,0 +1,28 @@
+from fastapi import FastAPI
+import json
+app = FastAPI()
+
+
+
+def load_data():
+    with open("patients.json", "r") as f:
+        data  = json.load(f)
+        
+    return data  
+
+@app.get("/")
+def hello():
+    return {"message": "Patient Managememnt System API"}
+
+
+
+@app.get("/about")
+def hello():
+    return {"message": " A fully functional API to manange your patient records"}
+
+
+@app.get("/view")
+def view():
+    data = load_data()
+    
+    return data
