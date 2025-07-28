0e4f15dd5874f5ffa32f7cd48df6e63587f356b5
Muhammad361738 <ullahehsan016@gmail.com>
2025-07-28 22:11:44 +0500

    update fast api

--- pydantic_crash_course/main.py
+++ pydantic_crash_course/main.py
@@ -1,8 +1,13 @@
-from pydantic import BaseModel
-
+from pydantic import BaseModel , EmailStr , Field
+from typing import List , Dict ,Optional
 class Patient(BaseModel):
-    name : str
-    age : int 
+    name : str = Field(max_length=50)
+    age : int = Field(gt=0, lt=120)
+    # email : EmailStr
+    weight : float = Field(gt=0)
+    married : bool
+    allergies : Optional[List[str]] = None
+    contact_details : Dict[str,str]
 
 def insert_patient (patient : Patient):
     print(patient.name)
@@ -15,7 +20,7 @@ def insert_patient (patient : Patient):
     print("Insert Data")
 
     
-patient_info = {"name":"Talha", "age":20}
+patient_info = {"name":"Talha", "age":20 , "weight": 75.5 , "married": True , "allergies":["pollen","Dust"] , "contact_details":{"email":"abs@gmail.com","phone":"0123456789"}}
 # patient_info_2 = {"name":"Talha", "age":"thirty"} # Error 
 
 patient_1 = Patient(**patient_info)
