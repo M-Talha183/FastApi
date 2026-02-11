cbab8f8d8fe30cf5a0ad739b9dd9d09368a75040
M-Talha183 <shahzainali5002@gmail.com>
2026-02-11 23:47:32 +0500

    Add new topic data valiadtion

--- Revesion/main.py
+++ Revesion/main.py
@@ -42,3 +42,10 @@ def read_item(item_id: int, q: str | None = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+# data types 
+def get_full_name(first_name, last_name):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+
