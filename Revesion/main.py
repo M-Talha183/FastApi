489abe79a1cd5beacc04a0ffcf60f259ca0c6409
M-Talha183 <shahzainali5002@gmail.com>
2026-02-11 23:48:48 +0500

    add new fastapi code

--- Revesion/main.py
+++ Revesion/main.py
@@ -49,3 +49,9 @@ def get_full_name(first_name, last_name):
     return full_name
 
 
+def get_full_name(first_name, last_name):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+
+print(get_full_name("john", "doe"))
