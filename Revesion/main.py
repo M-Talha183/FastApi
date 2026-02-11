8c45daa0c226f23d92aa86cd488e1b20ec3fe88e
M-Talha183 <shahzainali5002@gmail.com>
2026-02-11 23:26:03 +0500

    Add new topic data valiadtion

--- Revesion/main.py
+++ Revesion/main.py
@@ -13,3 +13,32 @@ def myitems():
 @app.get("/items/{item_id}")
 def get_dynamic_item(item_id : int , q : str | None = None):
     return {"item_id": item_id, "q": q}
+
+
+#  Data valaidation 
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool | None = None
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: str | None = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
