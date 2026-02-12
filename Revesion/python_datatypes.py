aaf89f76b1ec1f6c26609500a15840b416ae6907
M-Talha183 <shahzainali5002@gmail.com>
2026-02-12 23:48:26 +0500

    apply the code of data types

--- Revesion/python_datatypes.py
+++ Revesion/python_datatypes.py
@@ -1,1 +1,8 @@
 # TOday we explore data types for fastapi 
+
+def get_full_name(first_name, last_name):
+    full_name = first_name.title() + " " + last_name.title()
+    return full_name
+
+
+print(get_full_name("john", "doe"))
