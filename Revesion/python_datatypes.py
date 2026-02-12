1d7727fdba2085fdc7d3bc4db1ef68c94ebc9f52
M-Talha183 <shahzainali5002@gmail.com>
2026-02-13 01:08:58 +0500

    apply the code of data types

--- Revesion/python_datatypes.py
+++ Revesion/python_datatypes.py
@@ -6,3 +6,12 @@ def get_full_name(first_name, last_name):
 
 
 print(get_full_name("john", "doe"))
+
+
+
+# check the error 
+# def get_name_with_age(name: str , age :int):
+#     name_with_age = name + " is this old: " + age
+#     return name_with_age
+
+# print(get_name_with_age("talha","20"))
