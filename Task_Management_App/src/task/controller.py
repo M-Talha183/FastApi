8aeb23aec6ff676b3ac80d442e00b5a3e6f2b4c5
M-Talha183 <shahzainali5002@gmail.com>
2026-02-26 02:52:19 +0500

    Add get end point successfully

--- Task_Management_App/src/task/controller.py
+++ Task_Management_App/src/task/controller.py
@@ -2,6 +2,8 @@ from src.task.dtos import TaskSchema
 from src.task.models import TaskModel
 from sqlalchemy.orm import Session
 
+
+#  *********** create task ***********
 def create_task(body:TaskSchema , db:Session):
     data = body.model_dump()
     new_task = TaskModel(
@@ -13,3 +15,8 @@ def create_task(body:TaskSchema , db:Session):
     db.commit()
     db.refresh(new_task)
     return {"message": "Task created successfully!", "task": new_task}
+
+# *********** get all tasks ***********
+def get_all_tasks(db:Session):
+    tasks = db.query(TaskModel).all()
+    return {"tasks": tasks}
