8810c2357d58a083579ef4c7f6cdf6e0a11d9e2c
M-Talha183 <shahzainali5002@gmail.com>
2026-02-27 06:17:48 +0500

    CRUD complete Task Management App

--- Task_Management_App/src/task/controller.py
+++ Task_Management_App/src/task/controller.py
@@ -1,3 +1,5 @@
+from fastapi import HTTPException
+
 from src.task.dtos import TaskSchema
 from src.task.models import TaskModel
 from sqlalchemy.orm import Session
@@ -20,3 +22,37 @@ def create_task(body:TaskSchema , db:Session):
 def get_all_tasks(db:Session):
     tasks = db.query(TaskModel).all()
     return {"tasks": tasks}
+# *********** get task by id ***********
+def get_task_by_id(task_id:int, db:Session):
+    task = db.query(TaskModel).get(task_id)
+    if not task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    return {"message": "Task retrieved successfully!", "task": task}
+
+
+def update_task(task_id:int, body:TaskSchema, db:Session):
+    one_task = db.query(TaskModel).get(task_id)
+    if not one_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+ 
+    body_update = body.model_dump()
+    for field,value in body_update.items():
+        setattr(one_task, field, value)
+    
+    db.add(one_task)
+    db.commit()
+    db.refresh(one_task)
+    
+    return {"Status":"task update Succesfully ","data":one_task}
+    
+    
+    
+def delet_task(task_id:int, db:Session):
+    one_task = db.query(TaskModel).get(task_id)
+    if not one_task:
+        raise HTTPException(status_code=404, detail="Task not found")
+    
+    db.delete(one_task)
+    db.commit()
+    
+    return {"Status":"task delete Succesfully "}
--- Task_Management_App/src/task/routers.py
+++ Task_Management_App/src/task/routers.py
@@ -13,3 +13,17 @@ def create_task(body:TaskSchema , db = Depends(get_db)):
 @task_routes.get("/all_tasks")
 def get_all_tasks(db = Depends(get_db)):
     return controller.get_all_tasks(db)
+
+@task_routes.get("/one_task/{task_id}")
+def get_task_by_id(task_id:int, db = Depends(get_db)):
+    return controller.get_task_by_id(task_id, db)
+
+
+
+@task_routes.put("/update/{task_id}")
+def update_task(task_id:int, body:TaskSchema, db = Depends(get_db)):
+    return controller.update_task(task_id, body, db)
+
+@task_routes.delete("/delete/{task_id}")
+def delet_task(task_id:int, db = Depends(get_db)):
+    return controller.delet_task(task_id, db) 
